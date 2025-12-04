66ad180390fc6276cf98b84c73992fcbf0107d91
sunHelen12 <sunrise.developer12@gmail.com>
2025-12-04 12:39:52 -0300

    Concertando Engine

--- src/js/engine.js
+++ src/js/engine.js
@@ -61,7 +61,7 @@ var estadoLinhas = {};
      * Inicializa o estado das linhas do tabuleiro com base na configuração do XML.
      * @param {*} xmlDoc - Documento XML contendo a configuração do jogo. 
      */
-    inicializarLinhas: function(xmlDoc) {
+    function inicializarLinhas(xmlDoc) {
         // Extrai o número de linhas e colunas do tabuleiro a partir do XML
         let $tabuleiro = $(xmlDoc).find('tabuleiro');
         let numLinhas = parseInt($tabuleiro.attr('linhas'));
@@ -71,7 +71,7 @@ var estadoLinhas = {};
         for (let l = 0; l < numLinhas; l++) {
             for (let c = 0; c < numColunas - 1; c++) {
                 let id = `h-${l}-${c}`;
-                this.estadoLinhas[id] = { id: id, status: 'livre', dono: null };
+                estadoLinhas[id] = { id: id, status: 'livre', dono: null };
             }
         }
 
@@ -79,11 +79,11 @@ var estadoLinhas = {};
         for (let l = 0; l < numLinhas - 1; l++) {
             for (let c = 0; c < numColunas; c++) {
                 let id = `v-${l}-${c}`;
-                this.estadoLinhas[id] = { id: id, status: 'livre', dono: null };
+                estadoLinhas[id] = { id: id, status: 'livre', dono: null };
             }
         }
-        console.log("Linhas inicializadas na memória:", this.estadoLinhas);
-    },
+        console.log("Linhas inicializadas na memória:", estadoLinhas);
+    }
 
     /**
      * Processa uma jogada feita por um jogador.
@@ -92,14 +92,14 @@ var estadoLinhas = {};
      * @param {*} idJogador jogador que fez a jogada.
      * @returns {boolean} true se a jogada for válida, false caso contrário.
      */
-    processarJogada: function(idLinha, idJogador) {
-        let linha = this.estadoLinhas[idLinha];
+    function processarJogada (idLinha, idJogador) {
+        let linha = estadoLinhas[idLinha];
 
         // Se a linha não existe na memória, cria ela na hora
         if (!linha) {
             console.warn(`Linha ${idLinha} não encontrada na memória (Criando para teste)`);
-            this.estadoLinhas[idLinha] = { id: idLinha, status: 'livre', dono: null };
-            linha = this.estadoLinhas[idLinha];
+            estadoLinhas[idLinha] = { id: idLinha, status: 'livre', dono: null };
+            linha = estadoLinhas[idLinha];
         }
 
         if (linha && linha.status === 'livre') {
@@ -115,4 +115,3 @@ var estadoLinhas = {};
         console.log("Jogada inválida ou linha ocupada");
         return false; // Jogada inválida
     }
-};
